5ee812a24448518bc82f8efb947e9ec0732e8af2
Vimal-P-3692 <vimalp3692@gmail.com>
2025-12-23 21:18:05 +0530

    Update ...

--- Updated_Backend/routes/super-admin.routes.js
+++ Updated_Backend/routes/super-admin.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { registerSuperAdmin } = require('../controller/auth.controller');
 const router = express.Router();
 
-router.post("/login", (req, res) => {
+router.post("/signup", (req, res) => {
     const datas = req.body;
     const result  = registerSuperAdmin(datas);
     res.status(result.statusCode).json({ message: result.message });
