03d658dc5871f78d3bd4a2530bf31aceb4f29372
Vimal-P-3692 <vimalp3692@gmail.com>
2025-12-24 16:28:42 +0530

    Removed the rate limiting for now ...

--- Updated_Backend/server.js
+++ Updated_Backend/server.js
@@ -1,14 +1,12 @@
-// Importing the packages
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 
-// Values from .env file
 dotenv.config();
 const PORT = process.env.PORT;
 
-// Middleware to parse JSON requests
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
@@ -16,7 +14,7 @@ app.use(cookieParser());
 app.use("/api/super-admin/v1/auth", require("./routes/super-admin.routes"));
 app.use("/api/super-admin/v1", require("./routes/super-admin.routes"));
 
-// Starting the server
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
