da9358767f49160b5a9220d032913eb087906226
Vimal-P-3692 <vimalp3692@gmail.com>
2025-12-24 17:24:10 +0530

    Completed the handler for the insertion of the admins ...

---
+++ Updated_Backend/lambda/insert.js
@@ -0,0 +1,38 @@
+const { createAdmin } = require('../controller/insert.controller');
+const { verifyTokenFromEvent } = require('./middleware/verifyToken');
+
+// Handler for creating new admin
+module.exports.createAdminHandler = async (event) => {
+    const tokenVerification = verifyTokenFromEvent(event);
+
+    if (!tokenVerification.valid) {
+        return {
+            statusCode: 401,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify({
+                statusCode: 401,
+                message: tokenVerification.message
+            }),
+        };
+    }
+
+    const datas = JSON.parse(event.body);
+    const result = await createAdmin(datas);
+    
+    return {
+        statusCode: result.statusCode,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ 
+            statusCode: result.statusCode,
+            message: result.message
+        }),
+    };
+};
