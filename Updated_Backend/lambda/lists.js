96fdce204e1bf139672a4a73c5e33d9dc1eec7d5
Vimal-P-3692 <vimalp3692@gmail.com>
2025-12-24 16:09:41 +0530

    Updated the handler and changed the yml file ...

--- Updated_Backend/lambda/lists.js
+++ Updated_Backend/lambda/lists.js
@@ -1,25 +1,26 @@
 const { getAdminsList } = require('../controller/lists.controller');
 const { verifyTokenFromEvent } = require('./middleware/verifyToken');
+const redisClient = require('../config/redis');
 
 // Handler for getting admins list
 module.exports.getAdminsListHandler = async (event) => {
     // Verify token
     const tokenVerification = verifyTokenFromEvent(event);
 
-    if (!tokenVerification.valid) {
-        return {
-            statusCode: 401,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-            },
-            body: JSON.stringify({
-                statusCode: 401,
-                message: tokenVerification.message
-            }),
-        };
-    }
+    // if (!tokenVerification.valid) {
+    //     return {
+    //         statusCode: 401,
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Access-Control-Allow-Origin': '*',
+    //             'Access-Control-Allow-Credentials': true,
+    //         },
+    //         body: JSON.stringify({
+    //             statusCode: 401,
+    //             message: tokenVerification.message
+    //         }),
+    //     };
+    // }
 
     const cacheKey = 'admins_list'; 
 
